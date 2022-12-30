c4e16de062069a786c1292fa0199f6b6a13d9075
seanlalor <sla162@uclive.ac.nz>
2022-12-31 09:55:50 +1300

    Added an initalise file to the plane section of code

---
+++ EmbeddedPlane/Plane/planeInit.py
@@ -0,0 +1,13 @@
+"""This will be a module containing all of the initial setup for the embedded system this will tidy the main function
+    resulting. 
+    This will contain the initialisation for peripherals:
+    Main Engine
+    Wing / Tail Flaps
+    Wifi (I2C) Transmition (Recieving/Transmission)
+    Gyro
+    
+    Author: Sean Lalor
+    Start Date: 31/12/2022
+    """
+
+    #These are hardware dependent so will be left for now
